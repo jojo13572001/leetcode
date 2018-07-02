6c175a22298c9f32bd22d11b8aa302e0f356d033
QX350-PC\Bean <jojo13572001@yahoo.com.tw>
2018-07-02 18:47:13 +0800

    first version with bug "(])"

--- valid_parentheses/valid_parentheses/Solution.h
+++ valid_parentheses/valid_parentheses/Solution.h
@@ -1,9 +1,51 @@
 #pragma once
+//#include "stdafx.h"
 #include <String>
+#include <stack>
 
+using namespace std;
 class Solution {
 public:
-	bool isValid(std::string s) {
+	bool isValid(std::string s){
+		//create two strings
+		std::string leftParentheses = "({[";
+		std::string rightParentheses = ")}]";
+		//create stack
+		std::stack<char> sStack;
+		//repeat string size
+		for (unsigned int i = 0; i < s.size(); i++) {
+			std::cout << "string: "<<s[i]<<std::endl;
+			if (leftParentheses.find(s[i]) != std::string::npos) {
+				std::cout << "push " << s[i] << std::endl;
+				sStack.push(s[i]);
+			}
+			else if (rightParentheses.find(s[i] != std::string::npos)) {
+				if (sStack.empty()) {
+					std::cout << "stack empty" << std::endl;
+					return false;
+				}
+				else if (leftParentheses.find(sStack.top()) == rightParentheses.find(s[i])) {
+				std::cout << "pop" << s[i] << std::endl;
+					sStack.pop();
+				}
+			}
+			else {
+			//	std::cout << "no such character "<<s[i] << std::endl;
+				return false;
+			}
+		}
+		if (sStack.empty()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		//if 1. s[i] is left parnetheses, push		//push s[i] to stack, check top with s[i]
 
+		//else if 2. s[i] is right parentheses
+		//    - 2.1 stack is empty, fail
+		//	  - 2.2 top is right parentheses, fail
+		//	  - 2.3 top is left parentheses but not corresponding one, fail
+		//	  - 2.4 top is left parentheses and corresponding one, pop top
 	}
 }; 
